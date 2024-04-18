8e39e735f821791bb54ed7fd7d818081ca3c327d
zizu28 <ziblimzulka.zz@gmail.com>
2024-04-18 21:42:53 +0700

    Further logic addition

--- script.js
+++ script.js
@@ -1,73 +1,89 @@
-const gameBoard = {
-    board: [],
-    winningPotential: {
-        potential1: [1, 2, 3],
-        potential2: [1, 4, 7],
-        potential3: [2, 5, 8],
-        potential4: [3, 6, 9],
-        potential5: [1, 5, 9],
-        potential6: [3, 5, 7]
-    },
-    availableMoves: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-    symbols: ['X', 'O'],
-    players: ['player1', 'player2'],
-    player1Moves: [],
-    player2Moves: [],
-    movesPlayed: [] 
-}
+const Game = function(){
+    const gameBoard = {
+        board: [],
+        winningPatterns: {
+            pattern1: [1, 2, 3],
+            pattern2: [1, 4, 7],
+            pattern3: [2, 5, 8],
+            pattern4: [3, 6, 9],
+            pattern5: [1, 5, 9],
+            pattern6: [3, 5, 7]
+        },
+        availableMoves: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        symbols: ['X', 'O'],
+        players: ['player1', 'player2'],
+        player1Moves: [],
+        player2Moves: [],
+        movesPlayed: [] 
+    }
 
-// Assign each button an index
-const btn = document.querySelectorAll('.square');
-btn.forEach((b, index) => {
-    b.dataset.index = index + 1;
-})
+    const selectors = {
+        board: document.querySelector('.gameBoard'),
+        stat1: document.querySelector('.stat1'),
+        stat2: document.querySelector('.stat2'),
+        result: document.querySelector('.result'),
+        btn: document.querySelectorAll('.square')
+    }
 
-function play(player){
-    const board = document.querySelector('.gameBoard');
-    // const player1Text = document.querySelector('.stat1');
-    // const player2Text = document.querySelector('.stat2');
-    const result = document.querySelector('.result');
-    board.addEventListener('click', (e) => {
-        const clickedButton = e.target.closest('button');
-        const btnIndex = clickedButton.dataset.index;
-        if(clickedButton){
-            if(!gameBoard.movesPlayed.includes(btnIndex)){
-                gameBoard.movesPlayed.push(btnIndex);
-                gameBoard.availableMoves.splice(btnIndex, 1);
-                if(player === gameBoard.players[0]){
-                    clickedButton.textContent = gameBoard.symbols[0];
-                    gameBoard.player1Moves.push(btnIndex);
-                    result.textContent = `${gameBoard.players[1]}, your turn to play next!!!`; 
-                }
-                else{
-                    clickedButton.textContent = gameBoard.symbols[1];
-                    gameBoard.player2Moves.push(btnIndex);
-                    result.textContent = `${gameBoard.players[0]}, your turn to play next!!!`;
-                }
+    const btn = selectors.btn;
+    btn.forEach((b, index) => {
+        b.dataset.index = index + 1;
+    })
+
+    const patterns = Object.keys(gameBoard.winningPatterns);
+    const winDeterminant = function(){
+        for(const pattern in patterns){
+            if(gameBoard.player1Moves.sort().toString().includes(patterns[pattern].toString())){
+                selectors.result.textContent = 'Player 1 Wins!!!';
+                selectors.stat1.textContent = 'Winner';
+            }
+            else if(gameBoard.player2Moves.sort().toString().includes(patterns[pattern].toString())){
+                selectors.result.textContent = 'Player 2 Wins!!!';
+                selectors.stat2.textContent = 'Winner';
             }
             else{
-                if(player === gameBoard.players[0]){ 
-                    result.textContent = `Position ${btnIndex}, already played!!!`;
+                continue;
+            }
+        }
+    }
+    
+    const play = function(){
+        // let player = gameBoard.players[0];
+        const board = selectors.board;
+        const stat1 = selectors.stat1;
+        const stat2 = selectors.stat2;
+        const result = selectors.result;
+        board.addEventListener('click', (e) => {
+            const clickedButton = e.target.closest('button');
+            const btnIndex = clickedButton.dataset.index;
+            if(clickedButton){
+                if(!gameBoard.movesPlayed.includes(btnIndex)){
+                    gameBoard.movesPlayed.push(btnIndex);
+                    gameBoard.availableMoves.splice(btnIndex, 1);
+                    if(gameBoard.movesPlayed.length % 2 !== 0){
+                        stat2.textContent = '';
+                        clickedButton.textContent = gameBoard.symbols[0];
+                        gameBoard.player1Moves.push(btnIndex);
+                        stat1.textContent = 'Good play';
+                        result.textContent = `${gameBoard.players[1]}, your turn to play next!!!`; 
+                        winDeterminant();
+                    }
+                    else{
+                        stat1.textContent = '';
+                        clickedButton.textContent = gameBoard.symbols[1];
+                        gameBoard.player2Moves.push(btnIndex);
+                        stat2.textContent = 'Good play';
+                        result.textContent = `${gameBoard.players[0]}, your turn to play next!!!`;
+                        winDeterminant();
+                    }
                 }
                 else{
                     result.textContent = `Position ${btnIndex}, already played!!!`;
                 }
             }
-        }
-        
-    })
-}
+        })
+    }
+    return { play, gameBoard, selectors }
+}()
+Game.play();
 
-// while(gameBoard.availableMoves.length !== 0){
-//     for(const potential in gameBoard.winningPotential){
-//         if(gameBoard.movesPlayed.contains(potential) ){
-            
-//         }
-//     }
-//     // gameBoard.winningPotential
-//     // if(!gameBoard.movesPlayed.contains(gameBoard.winningPotential.) || !gameBoard.movesPlayed.contains(gameBoard.player2Moves)){
-    
-//     gameBoard.availableMoves.length--;
-// }
-
-play('player2');
