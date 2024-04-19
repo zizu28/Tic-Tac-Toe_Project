ed899db95396dae9a503caa537aa1137d660667e
zizu28 <ziblimzulka.zz@gmail.com>
2024-04-19 21:10:27 +0700

    Further logic resolution

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ const Game = function(){
     const gameBoard = {
         board: [],
         winningPatterns: {
-            pattern1: [1, 2, 3],
-            pattern2: [1, 4, 7],
-            pattern3: [2, 5, 8],
-            pattern4: [3, 6, 9],
-            pattern5: [1, 5, 9],
-            pattern6: [3, 5, 7]
+            pattern1: [0, 1, 2],
+            pattern2: [0, 3, 6],
+            pattern3: [1, 4, 7],
+            pattern4: [2, 5, 8],
+            pattern5: [0, 4, 8],
+            pattern6: [2, 4, 6]
         },
         availableMoves: [0, 1, 2, 3, 4, 5, 6, 7, 8],
         symbols: ['X', 'O'],
         players: ['player1', 'player2'],
         player1Moves: [],
         player2Moves: [],
-        movesPlayed: [] 
+        movesPlayed: [],
+        player1Wins: [],
+        player2Wins: [] 
     }
 
     const selectors = {
@@ -22,7 +24,9 @@ const Game = function(){
         stat1: document.querySelector('.stat1'),
         stat2: document.querySelector('.stat2'),
         result: document.querySelector('.result'),
-        btn: document.querySelectorAll('.square')
+        btn: document.querySelectorAll('.square'),
+        startGame: document.querySelector('.start'),
+        resetGame: document.querySelector('.reset')
     }
 
     const btn = selectors.btn;
@@ -30,60 +34,83 @@ const Game = function(){
         b.dataset.index = index + 1;
     })
 
-    const patterns = Object.keys(gameBoard.winningPatterns);
     const winDeterminant = function(){
-        for(const pattern in patterns){
-            if(gameBoard.player1Moves.sort().toString().includes(patterns[pattern].toString())){
-                selectors.result.textContent = 'Player 1 Wins!!!';
-                selectors.stat1.textContent = 'Winner';
-            }
-            else if(gameBoard.player2Moves.sort().toString().includes(patterns[pattern].toString())){
-                selectors.result.textContent = 'Player 2 Wins!!!';
-                selectors.stat2.textContent = 'Winner';
-            }
-            else{
-                continue;
+        if(gameBoard.availableMoves.length === 0){
+            for(const pattern in gameBoard.winningPatterns){
+                if(gameBoard.player1Moves.sort().toString().includes(pattern.toString())){
+
+                }
             }
         }
     }
     
+    // Game logic
     const play = function(){
         // let player = gameBoard.players[0];
         const board = selectors.board;
         const stat1 = selectors.stat1;
         const stat2 = selectors.stat2;
         const result = selectors.result;
+        let counter = 0;
         board.addEventListener('click', (e) => {
             const clickedButton = e.target.closest('button');
-            const btnIndex = clickedButton.dataset.index;
+            let btnIndex = Number(clickedButton.dataset.index) - 1;
+            if(gameBoard.availableMoves.length < btnIndex){
+                btnIndex -= counter;
+            }
             if(clickedButton){
                 if(!gameBoard.movesPlayed.includes(btnIndex)){
-                    gameBoard.movesPlayed.push(btnIndex);
-                    gameBoard.availableMoves.splice(btnIndex, 1);
+                    gameBoard.movesPlayed.push(gameBoard.availableMoves[btnIndex]);
                     if(gameBoard.movesPlayed.length % 2 !== 0){
                         stat2.textContent = '';
                         clickedButton.textContent = gameBoard.symbols[0];
-                        gameBoard.player1Moves.push(btnIndex);
+                        gameBoard.player1Moves.push(gameBoard.availableMoves[btnIndex]);
                         stat1.textContent = 'Good play';
                         result.textContent = `${gameBoard.players[1]}, your turn to play next!!!`; 
-                        winDeterminant();
                     }
-                    else{
+                    else if(gameBoard.movesPlayed.length % 2 === 0){
+                        gameBoard.player2Moves.push(gameBoard.availableMoves[btnIndex]);
                         stat1.textContent = '';
                         clickedButton.textContent = gameBoard.symbols[1];
-                        gameBoard.player2Moves.push(btnIndex);
                         stat2.textContent = 'Good play';
                         result.textContent = `${gameBoard.players[0]}, your turn to play next!!!`;
-                        winDeterminant();
                     }
+                    gameBoard.availableMoves.splice(btnIndex, 1);
                 }
                 else{
                     result.textContent = `Position ${btnIndex}, already played!!!`;
                 }
+                counter++;
             }
         })
     }
-    return { play, gameBoard, selectors }
+
+    // Start the game
+    selectors.startGame.addEventListener('click', () => {
+        selectors.result.textContent = '';
+        btn.forEach(b => {
+            b.disabled = false;
+        })
+    });
+
+    // Reset the game
+    const reset = function(){
+        selectors.resetGame.addEventListener('click', () => {
+            selectors.result.textContent = 'Click "Start Button" to begin the game';
+            selectors.stat1.textContent = '';
+            selectors.stat2.textContent = '';
+            gameBoard.movesPlayed.splice(0, gameBoard.movesPlayed.length);
+            gameBoard.availableMoves.splice(0, gameBoard.availableMoves.length);
+            gameBoard.availableMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+            btn.forEach(b => {
+                b.textContent = '';
+                b.disabled = true;
+            })
+        });
+    }
+    
+    return { play, reset, gameBoard, selectors }
 }()
 Game.play();
+Game.selectors.resetGame.onclick = Game.reset();
 
